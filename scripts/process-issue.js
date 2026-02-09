2ca23d196b9fe8c82ec551b2cd0779aed55bab74
杉本光一 <sugimotokoichi@sugimotokouichinoMacBook-Pro.local>
2026-02-09 17:04:01 +0900

    Fix: Remove backticks in workflow comment to avoid shell execution error

--- scripts/process-issue.js
+++ scripts/process-issue.js
@@ -20,9 +20,9 @@ async function main() {
     employees = JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
   }
 
-  // タイトルの接頭辞で処理を分岐
-  const isUpdate = issueTitle.startsWith('[Career]');
-  const isDelete = issueTitle.startsWith('[Delete]');
+  // ラベルによって処理を分岐
+  const isUpdate = issueLabels.some(l => l.name === 'employee-update');
+  const isDelete = issueLabels.some(l => l.name === 'employee-delete');
 
   if (isUpdate) {
     const data = parseIssueBody(issueBody);
